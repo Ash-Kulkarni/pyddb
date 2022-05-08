778a3f94ed8282714684c2eaeadf51a006aa7a7e
Ash-Kulkarni <ash.kulkarni@arup.com>
2022-05-08 21:54:41 +0100

    added dependencies

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup, find_packages
 
 VERSION = "0.0.1"
 DESCRIPTION = "DDB API Client"
-LONG_DESCRIPTION = "Python interface for interacting with the Digital Design Brief REST API."
+LONG_DESCRIPTION = (
+    "Python interface for interacting with the Digital Design Brief REST API."
+)
 
-# Setting up
+
 setup(
-    # the name must match the folder name 'verysimplemodule'
     name="pyddb",
     version=VERSION,
     author="Ash Kulkarni",
@@ -14,15 +15,8 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=[
-        "pydantic",
-        "aiohttp",
-        "asyncio",
-    ],
-    dependency_links=[
-        "https://github.com/arup-group/ddbpy_auth/tarball/master"
-    ],  # add any additional packages that
-    # needs to be installed along with your package. Eg: 'caer'
+    install_requires=["pydantic", "aiohttp", "asyncio", "pandas"],
+    dependency_links=["https://github.com/arup-group/ddbpy_auth/tarball/master"],
     keywords=["python", "ddb", "digital", "design", "brief", "client", "api"],
     classifiers=[
         "Development Status :: 3 - Alpha",
